2bb3ad15e7953afc98ea8382d87f07cb577a8f6c
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-22 18:47:10 +0100

    add a some styling to VideoItem and VideoList, create a VideoItem css stylesheet

--- youtube-app/src/components/VideoList.js
+++ youtube-app/src/components/VideoList.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import VideoItem from './VideoItem';
+import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = ({videos}) => { //use destructuring to get videos from props
+//use destructuring to get videos from props
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
 
-    const renderedList = videos.map((video) =>{
-        return <VideoItem video={video} />;
-    });
-
-    return(
-        <div>{renderedList}</div>
-    );
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
