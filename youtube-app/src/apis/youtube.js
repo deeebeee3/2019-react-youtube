a8a50fc2e89ae48c54b5e5856ee887c8d1b30503
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-22 17:33:19 +0100

    youtube API config

--- youtube-app/src/apis/youtube.js
+++ youtube-app/src/apis/youtube.js
@@ -1,1 +1,10 @@
 const KEY = 'AIzaSyBV73CDTcohnaXNqZoMsYEpKrkWabdzvqA';
+
+export default AudioContextOptions.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params:{
+        part : 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
