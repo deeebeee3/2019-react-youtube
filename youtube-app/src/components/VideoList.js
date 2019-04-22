e0ff0efc1a8c6ae0e20e65f971bed913a3e86152
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-22 20:49:42 +0100

    give iframe a title and fix VideoList missing key prop error

--- youtube-app/src/components/VideoList.js
+++ youtube-app/src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from "./VideoItem";
 //use destructuring to get videos from props
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>;
