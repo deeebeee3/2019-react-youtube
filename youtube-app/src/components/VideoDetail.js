92ee91cee8bd7623ddfaddef850039dcc4729bd6
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-22 20:19:54 +0100

    fix issue with selectedVideo state objects default value being null by placing a check in VideoDetail. Conditional rendering...

--- youtube-app/src/components/VideoDetail.js
+++ youtube-app/src/components/VideoDetail.js
@@ -2,6 +2,9 @@ import React from "react";
 
 //use destructuring to get videos from props
 const VideoDetail = ({ video }) => {
+    if(!video){
+        return <div>Loading...</div>;
+    }
   return <div>{video.snippet.title}</div>;
 };
 
